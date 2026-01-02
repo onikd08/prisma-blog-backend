60a87630f3d367c7e6dfe3c7849960b713764e2f
onikd08 <onikd08@gmail.com>
2026-01-02 06:20:18 +0200

    Add better-auth setup and addtional fields in user model

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import postRouter from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors";
 
 const app = express();
 
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use("/posts", postRouter);
 
---
+++ src/lib/auth.ts
@@ -0,0 +1,34 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+  trustedOrigins: ["http://localhost:3000"],
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "USER",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "ACTIVE",
+        required: false,
+      },
+    },
+  },
+
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+  },
+});
