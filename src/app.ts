5d30a1e4d397ddf441198c59e59f754db835e6f8
onikd08 <onikd08@gmail.com>
2026-01-02 05:14:54 +0200

    Setup express server

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Hello World");
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const port = process.env.PORT || 3000;
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to database successfully");
+
+    app.listen(port, () => {
+      console.log("Server is running on port", port);
+    });
+  } catch (error) {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
